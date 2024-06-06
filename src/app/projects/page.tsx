20fac51e5cab2bb313861dd0b24dbcfef3e6b352
CnoLvaN <anordgame@gmail.com>
2024-06-06 17:25:58 +0200

    project dom

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,3 +1,9 @@
-export default function Projects() {
-  return <h1>See you</h1>;
+import styles from "../base.module.scss";
+
+export default function Home() {
+  return (
+    <div className={styles.img}>
+      <h1>Projects</h1>
+    </div>
+  );
 }
