af44f24cb3e77820488c64de3d052de855fafd8d
CnoLvaN <anordgame@gmail.com>
2024-06-07 16:17:25 +0200

    -test-

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,12 +1,33 @@
-import { Button } from "antd";
+import { Button, Card } from "antd";
 import styles from "../base.module.scss";
+import { Container } from "@/components/Container/Container";
+const { Meta } = Card;
 
 export default function Home() {
   return (
-    <div>
+    <Container>
       <div className={styles.tittle}>
-        <h1>Projects</h1>
+        <h1>Last News</h1>
       </div>
+
+      <ul className={styles.list}>
+        <li>
+          <Card
+            style={{ width: 320 }}
+            cover={<img alt="example" src="/news-images/news1.jpg" />}
+          >
+            <h1>Last Interview</h1>
+          </Card>
+        </li>
+        <li>
+          <Card
+            style={{ width: 320 }}
+            cover={<img alt="example" src="/news-images/news2.jpg" />}
+          >
+            <h1>Last Interview</h1>
+          </Card>
+        </li>
+      </ul>
 
       <div className={styles.button}>
         <a href="/">
@@ -15,6 +36,6 @@ export default function Home() {
           </Button>
         </a>
       </div>
-    </div>
+    </Container>
   );
 }
