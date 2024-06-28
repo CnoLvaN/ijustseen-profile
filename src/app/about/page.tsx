3f0af92d88a1949ce2441d860c7a4b3e8edf27cc
CnoLvaN <anordgame@gmail.com>
2024-06-28 11:26:13 +0200

    aboutPage

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,13 +1,14 @@
 import Image from "next/image";
 import styles from "../base.module.scss";
 import { Button, Typography } from "antd";
+import cn from "classnames";
 
 export default function Home() {
   const { Title } = Typography;
   return (
     <main>
       <>
-        <div className={styles.tittle}>
+        <div className={cn(styles.tittle)}>
           <h1>About Me</h1>
         </div>
 
@@ -17,7 +18,20 @@ export default function Home() {
 
         <div className={styles.tittle}>
           <h2>Andrew Eroshenkov</h2>
-          <p>React, Next, Vue, Python, C#, Blueprints, etc.</p>
+        </div>
+
+        <div className={cn(styles.aboutParagraph)}>
+          <p className={styles.white}>
+            {
+              "Hello there! I am 17 yo and as you notice - I am Andrew. I'm studying in Jovan Jovanovic Zmaj Gymnasium on 2nd year. "
+            }
+            <span className={styles.pastWhite}></span>
+          </p>
+          <h3 className={styles.tittle}>Skills</h3>
+          <p className={styles.white}>
+            React, Next, Vue, Python, C#, Blueprints, etc.
+            <span className={styles.pastWhite}></span>
+          </p>
         </div>
 
         <div className={styles.button}>
